b4a6161068b429b06ef293537d07152f38c7239f
Abhaycodes <abhayvandana123@gmail.com>
2026-05-29 00:56:59 +0530

    Add files via upload

---
+++ Digit-Manipulation/Pair-Logic/Palindrome-Logic/count_odd_digits_by functions.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int rev(int n)
+{
+	int digit;int count =0;
+	for(;n>0;n=n/10)
+	{
+		digit=n%10;
+		if(digit%2!=0)
+		{
+			count++;
+			
+		}
+	}
+	return count;
+	
+}
+int main()
+{
+	int n;int final;
+	scanf("%d",&n);
+	final=rev(n);
+	printf("%d",final);
+	
+}
